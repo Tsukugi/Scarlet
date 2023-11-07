23e9cdcf334d4dc0c2947305e99ea50587d68957
Tsukugi <Tsukugi@proton.me>
2023-11-07 01:14:08 +0100

    Working version v1.0.1

--- src/repo.ts
+++ src/repo.ts
@@ -1,3 +1,4 @@
+import * as fetch from "node-fetch";
 import {
     Sort,
     Book,
@@ -5,7 +6,15 @@ import {
     Pagination,
     useAPILoader,
     LilithRepo,
+    CustomFetch,
+    CustomFetchResponse,
+    CloudFlareConfig,
+    UseDomParser,
+    UseDomParserImpl,
+    Attributes,
+    ElementProps,
 } from "@atsu/lilith";
+import { load, Cheerio, AnyNode } from "cheerio";
 
 const handleLilithErrors = <T>(req: Promise<T>): Promise<T | null> =>
     req.catch((err) => {
@@ -13,10 +22,20 @@ const handleLilithErrors = <T>(req: Promise<T>): Promise<T | null> =>
         return null;
     });
 
+export const cookies: CloudFlareConfig = {
+    "User-Agent":
+        "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/118.0",
+    cookie: "cf_clearance=No07bTPjTPG8ay4yw6Swd2YWsl3OOQEyUD5k3CrfLV0-1698867081-0-1-6fa998a8.7d6487d8.38a8c748-160.0.0; csrftoken=GVZOyHvhqPkKd294OAxMu2szdWS9pbU4Pp6uHPOQ2EbDjzTlePBtaF3aF8kNVnNV",
+};
+
 export const useLilithLoader = () => {
     const lilithLoader = useAPILoader({
         repo: LilithRepo.NHentai,
-        configurations: {},
+        configurations: {
+            fetchImpl: customFetchImpl,
+            domParser: useCheerioDomParser,
+            headers: cookies,
+        },
     });
 
     const get = async (identifier: string): Promise<Book | null> => {
@@ -45,4 +64,45 @@ export const useLilithLoader = () => {
         paginate,
         random,
     };
+};
+
+export const customFetchImpl: CustomFetch = async (
+    url,
+    options,
+): Promise<CustomFetchResponse> => {
+    const res = await fetch.default(url, options as any);
+    return {
+        text: () => res.text(), // We need this to avoid fetch/node-fetch "cannot find property 'disturbed' from undefined"
+        json: <T>() => res.json() as T,
+        status: res.status,
+    };
+};
+
+export const useCheerioDomParser: UseDomParser = (stringDom: string) => {
+    const $ = load(stringDom);
+
+    const parser = (el: Cheerio<AnyNode>): UseDomParserImpl => {
+        const find = (query: string) => parser(el.find(query).first());
+        const findAll = (query: string) =>
+            el
+                .find(query)
+                .map((_, element) => parser($(element)))
+                .get();
+        const getElement = (): ElementProps => {
+            const attributes: Partial<Attributes> = {
+                href: el.attr("href") || "",
+                "data-src": el.attr("data-src") || "",
+                width: parseInt(el.attr("width") || "0", 10),
+                height: parseInt(el.attr("height") || "0", 10),
+            };
+            return {
+                textContent: el.text(),
+                attributes,
+            };
+        };
+
+        return { find, findAll, getElement };
+    };
+
+    return parser($("html"));
 };
